1ecd643ee1415a8c0e1e072ff9b937088ec30141
Wenchel <WenchelChow@outlook.com>
2021-02-22 17:26:16 +0800

    添加用户实体。

---
+++ Blog.Shared/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Shared.Entities
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserNickname { get; set; }
+        public string UserAvatar { get; set; }
+        public string UserPassword { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhone { get; set; }
+        public Group UserGroup { get; set; }
+    }
+    public enum Group
+    { 
+        Administrator,
+        User
+    }
+}
