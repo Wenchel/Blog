50b8334ba558f268fa89c7204274625173a76f88
Wenchel <WenchelChow@outlook.com>
2021-02-24 17:19:36 +0800

    完成注册功能的前端设计。

---
+++ Blog.Client/Blog.Client.Shared/PageBases/RegisterBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntDesign;
+using Blog.Shared.Parameters;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Blog.Client.Shared.PageBases
+{
+    public class RegisterBase : ComponentBase
+    {
+        public string VerificationCodeButtonText { get; set; } = "获取验证码";
+        public Button VerificationCodeButton { get; set; }
+        public UserService_SignUpPara registerUser = new UserService_SignUpPara();
+
+        public void OnFinish()
+        {
+            Console.WriteLine($"Success:");
+        }
+
+        public async Task SendVerificationCodeAsync()
+        {
+            VerificationCodeButton.Disabled = true;
+            for (int i = 0; i < 60; i++)
+            {
+                await Task.Delay(1000);
+                VerificationCodeButtonText = (59 - i).ToString();
+                StateHasChanged();
+            }
+            VerificationCodeButtonText= "获取验证码";
+            VerificationCodeButton.Disabled = false;
+
+        }
+
+    }
+}
--- Blog.Shared/Parameters/UserServiceParas.cs
+++ Blog.Shared/Parameters/UserServiceParas.cs
@@ -1,19 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Blog.Shared.Parameters
 {
     public class UserServiceParaBase
     {
+        [Display(Name = "邮箱")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [EmailAddress(ErrorMessage ="{0}是不合法的")]
         public string UserEmail { get; set; }
     }
 
     public class UserService_IsExistPara : UserServiceParaBase { }
 
-    public class UserService_SignUpPara : UserServiceParaBase {
+    public class UserService_SignUpPara : UserServiceParaBase 
+    {
+        
+        [Display(Name ="昵称")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [MaxLength(10, ErrorMessage = "{0}长度不能超过{1}")]
         public string UserNickname { get; set; }
+
+        [Display(Name = "密码")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [MinLength(8,ErrorMessage ="{0}至少8位")]
         public string UserPassword { get; set; }
+
+        [Display(Name = "确认密码")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [Compare("UserPassword",ErrorMessage ="{0}和{1}不一致")]
         public string UserRePassword { get; set; }
     }
 }
