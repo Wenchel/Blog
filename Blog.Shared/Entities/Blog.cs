7c71d29f50ca64e925fb891cc46130006bb9377f
Wenchel@home <WenchelChow@outlook.com>
2021-02-26 23:51:26 +0800

    更新博客实体。

--- Blog.Shared/Entities/Blog.cs
+++ Blog.Shared/Entities/Blog.cs
@@ -17,10 +17,29 @@ namespace Blog.Shared.Entities
         /// 博客标题
         /// </summary>
         public string BlogTitle { get; set; }
-
+        /// <summary>
+        /// 博客分类ID
+        /// </summary>
         public Guid ClassificationId { get; set; }
-
+        /// <summary>
+        /// 博客所在分类
+        /// </summary>
         [Navigate(nameof(ClassificationId))]
         public BlogClassification Classification { get; set; }
+
+        [Column(StringLength = -1)]//映射为text类型
+        public string BlogContent { get; set; }
+
+        /// <summary>
+        /// 创建博客时间
+        /// </summary>
+        [Column(ServerTime = DateTimeKind.Utc, CanUpdate = false)]
+        public DateTime CreateBlogTime { get; set; }
+
+        /// <summary>
+        /// 创建更新时间
+        /// </summary>
+        [Column(ServerTime = DateTimeKind.Utc)]
+        public DateTime UpdateBlogTime { get; set; }
     }
 }
