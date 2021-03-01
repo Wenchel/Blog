3dd89201e0edaf730d54f73309174038b7753239
Wenchel <WenchelChow@outlook.com>
2021-03-01 16:40:18 +0800

    添加登录页面；将Pro模板样式引入全局样式。

---
+++ Blog.Client/Blog.Client.Shared/PageBases/SignInBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Shared.Parameters;
+using Microsoft.AspNetCore.Components;
+
+namespace Blog.Client.Shared.PageBases
+{
+    public class SignInBase: ComponentBase
+    {
+        public UserService_SignInPara SignInUser { get; set; } = new UserService_SignInPara();
+    }
+}
